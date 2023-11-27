07d27bd61cabbdf0dff257f12bdae4f987424316
gunjoong-kim <rjsoownd0419@gmail.com>
2023-11-27 12:35:02 +0900

    init

---
+++ srcs/requirements/mariadb/conf/create_wordpressdb_user.sql
@@ -0,0 +1,11 @@
+-- database 생성
+create database wordpress;
+
+-- database 유저생성
+create user 'wordpressuser'@'%' identified by '{ja164741}';
+
+-- database 권한
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES, CREATE VIEW, EVENT, TRIGGER, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, EXECUTE ON wordpress.* TO 'wordpressuser'@'%';
+
+-- 적용
+FLUSH PRIVILEGES;
